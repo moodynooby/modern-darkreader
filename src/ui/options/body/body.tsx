2fb29bea36b04a781eb221e7b007f4ecce688c67
Manas Doshi <98732831+moodynooby@users.noreply.github.com>
2026-01-19 20:40:55 +0530

    feat: Redesign options page header with contact and support buttons, update version to 3.1.1, and add a new Firefox debug script.

--- src/ui/options/body/body.tsx
+++ src/ui/options/body/body.tsx
@@ -23,8 +23,19 @@ export default function Body(props: BodyProps): Malevic.Child {
     return (
         <body>
             <header>
-                <h1 id="title">Lean Dark+</h1>
-                <h1 id="title">Settings</h1>
+                <div class="header-main">
+                    <h1 id="title">Lean Dark+ Settings</h1>
+                </div>
+                <div className="header-buttons">
+                    <a href="mailto:immanasdoshi@gmail.com" target="_blank" class="header-button" role="button">
+                        <svg class="header-button__icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>
+                        <span>Contact</span>
+                    </a>
+                    <a href="https://ko-fi.com/U7U51S87CX" target="_blank" class="header-button header-button--coffee" role="button">
+                        <svg class="header-button__icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z" /></svg>
+                        <span>Support</span>
+                    </a>
+                </div>
             </header>
             <TabPanel
                 activeTabId={store.activeTabId}
